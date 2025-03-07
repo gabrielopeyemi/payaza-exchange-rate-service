2454d7db64d2f57cd8da0dfd7be419c19120cd5d
gabrielopeyemi <famosipe2010@gmail.com>
2025-03-07 15:52:04 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,13 @@
 import { useState } from "react";
-import ExchangeRateTable from "./component/table";
-import { ExchangeRate, SCREEN } from "./utility/types";
-import Update from "./page/update";
 
-function App() {
-  const [screen, setScreen] = useState<SCREEN>(SCREEN.HOME);
-  const [country, setCountry] = useState<ExchangeRate | any>(null);
-
-  const handleOpenUpdate = (country: string) => {
-    setCountry(country);
-    setScreen(SCREEN.UPDATE);
-  };
-
+const App = () => {
   return (
-    <>
-      {screen === SCREEN.HOME && (
-        <>
-          <ExchangeRateTable handleOpenUpdate={handleOpenUpdate} />
-          {/* <button
-            onClick={() => setScreen(SCREEN.CREATE)}
-            className="w-8 h-8 bg-[#333] absolute bottom-2 md:bottom-6 right-2 md:right-6 flex justify-center text-align items-center rounded-full"
-          >
-            <FiPlus color="#fff" />
-          </button> */}
-        </>
-      )}
-      {/* {screen === SCREEN.CREATE && (
-        <>
-          <Create setScreen={setScreen} />
-        </>
-      )} */}
-
-      {screen === SCREEN.UPDATE && (
-        <>
-          <Update setScreen={setScreen} country={country} />
-        </>
-      )}
-    </>
+    <div className="text-center my-40">
+      <h1>Start vite + typescript</h1>
+      <p>
+        Edit <code>App.tsx</code> and save to test HMR updates.
+      </p>
+    </div>
   );
 }
 
